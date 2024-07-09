3284dd42cd2fba66192c701d3e64ddf98f2b84b4
AkiPoh <59505647+AkiPoh@users.noreply.github.com>
2024-07-09 17:17:42 +0300

    Add rock paper scissor button that call playRound when pressed

--- README.md
+++ README.md
@@ -39,3 +39,20 @@ rock wins scissors
 paper loses scissors
 
 everything else is a tie
+
+### Dev stage 2
+9.7.2024-
+
+Add UI, and make necessary modifcations to app.
+- [x] Set up GIT branch for rps-ui
+- [x] Add "rock", "paper" and "scissors" buttons
+    - Add event listeners that calls playRound() with correct playerSelection
+- [ ] Add div for displaying results
+    - Remove console.log() and replace them with DOM methods
+- [ ] Display running score, and announce winner after computer or player reaches 5 points
+- [ ] Merge rps-ui with main
+    1. git checkout mian
+    2. git merge rps-ui
+    3. git push origin main
+    4. git branch -d rps-ui
+    5. git push origin --delete rps-ui
--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-const ROCK = "rock"
-const PAPER = "paper"
-const SCISSORS = "scissors"
-const HUMAN = "human"
-const MACHINE = "machine"
 
-let humanScore = 0
-let computerScore = 0
 
-function getComputerChoice() {
+function getMachineChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     switch(randomNumber) {
         case 0:
@@ -39,37 +32,36 @@ function getHumanChoice() {
     }
 }
 
-function decideWinner (humanChoice, computerChoice) {
-    if (humanChoice === ROCK && computerChoice === PAPER) {
+function decideWinner (humanChoice, machineChoice) {
+    if (humanChoice === ROCK && machineChoice === PAPER) {
         return MACHINE;
-    } else if (humanChoice === PAPER && computerChoice === ROCK) {
+    } else if (humanChoice === PAPER && machineChoice === ROCK) {
         return HUMAN;
-    } else if (humanChoice === ROCK && computerChoice === SCISSORS) {
+    } else if (humanChoice === ROCK && machineChoice === SCISSORS) {
         return HUMAN;
-    } else if (computerChoice === ROCK && humanChoice === SCISSORS) {
+    } else if (machineChoice === ROCK && humanChoice === SCISSORS) {
         return MACHINE;
-    } else if (humanChoice === SCISSORS && computerChoice === PAPER) {
+    } else if (humanChoice === SCISSORS && machineChoice === PAPER) {
         return HUMAN;
-    } else if (computerChoice === SCISSORS && humanChoice === PAPER) {
+    } else if (machineChoice === SCISSORS && humanChoice === PAPER) {
         return MACHINE;
     }
     return("tie");
 }
 
-function playRound(roundNumber) {
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-    let winner = decideWinner(humanChoice, computerChoice);
+function playRound(humanChoice, roundNumber = "roundNumberUnspecified") {
+    let machineChoice = getMachineChoice();
+    let winner = decideWinner(humanChoice, machineChoice);
     if (winner === HUMAN) {
         ++humanScore;
-        console.log(`You won, round ${roundNumber}!\nYour score: ${humanScore}.\nMachine score: ${computerScore}\nYou chose ${humanChoice}, the machine chose ${computerChoice}.`);
+        console.log(`You won, round ${roundNumber}!\nYour score: ${humanScore}.\nMachine score: ${machineScore}\nYou chose ${humanChoice}, the machine chose ${machineChoice}.`);
         return;
     } else if (winner === MACHINE) {
-        ++computerScore;
-        console.log(`You lost, round ${roundNumber}!\nYour score: ${humanScore}.\nMachine score: ${computerScore}\nYou chose ${humanChoice}, the machine chose ${computerChoice}.`);
+        ++machineScore;
+        console.log(`You lost, round ${roundNumber}!\nYour score: ${humanScore}.\nMachine score: ${machineScore}\nYou chose ${humanChoice}, the machine chose ${machineChoice}.`);
         return;
     } else {
-        console.log(`It's a tie, round ${roundNumber}!\nYour score: ${humanScore}.\nMachine score: ${computerScore}\nYou chose ${humanChoice}, the machine chose ${computerChoice}.`);
+        console.log(`It's a tie, round ${roundNumber}!\nYour score: ${humanScore}.\nMachine score: ${machineScore}\nYou chose ${humanChoice}, the machine chose ${machineChoice}.`);
         return;
     }
 }
@@ -80,18 +72,33 @@ function playGame() {
         playRound(roundNumber);
     }
     roundNumber--;  
-    if (humanScore > computerScore) {
-        console.log(`You won, the game of ${roundNumber} rounds!\nYour score: ${humanScore}.\nMachine score: ${computerScore}`);
-    } else if (computerScore > humanScore) {
-        console.log(`You lost, the game of ${roundNumber} rounds!\nYour score: ${humanScore}.\nMachine score: ${computerScore}`);
+    if (humanScore > machineScore) {
+        console.log(`You won, the game of ${roundNumber} rounds!\nYour score: ${humanScore}.\nMachine score: ${machineScore}`);
+    } else if (machineScore > humanScore) {
+        console.log(`You lost, the game of ${roundNumber} rounds!\nYour score: ${humanScore}.\nMachine score: ${machineScore}`);
     } else {
-        console.log(`The game is a tie, of ${roundNumber} rounds!\nYour score: ${humanScore}.\nMachine score: ${computerScore}`);
+        console.log(`The game is a tie, of ${roundNumber} rounds!\nYour score: ${humanScore}.\nMachine score: ${machineScore}`);
     }
 }
 
+const ROCK = "rock"
+const PAPER = "paper"
+const SCISSORS = "scissors"
+const HUMAN = "human"
+const MACHINE = "machine"
+
+let humanScore = 0
+let machineScore = 0
+let roundNumber = 0;
+
+const rockButton = document.querySelector("#rockButton");
+const paperButton = document.querySelector("#paperButton");
+const scissorsButton = document.querySelector("#scissorsButton");
+
 console.log("Welome to playing Rock Paper Scissors!");
 
-//console.log(getComputerChoice());
-//console.log(getHumanChoice());
 
-playGame();
+
+rockButton.addEventListener("click", () => {roundNumber++; playRound("rock", roundNumber);});
+paperButton.addEventListener("click", () => {roundNumber++; playRound("paper", roundNumber);});
+scissorsButton.addEventListener("click", () => {roundNumber++; playRound("scissors", roundNumber);});
